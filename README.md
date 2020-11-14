985c040c3046554987402378d20ac36727424905
Ivan Robles <navitux@disroot.org>
2020-11-14 03:00:58 -0600

    First Commit/Primer Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+#ESPAÑOL:
+---
+##Vista General
+
+Workshops es un proyecto implementado en Python (versión 3.8.x) usando Django (versión 2.2.x) (basado en una implementación anterior en PHP) que consiste en un proyecto compuesto de aplicaciones que respectivamente: Permitan la gestión de listas de estudiantes en cursos que a su vez están clasificados por departamentos, Permita iniciar sesión como estudiante, profesor o administrador, y una que muestre un panel de control o pagina de inicio según el rol que tenga cada usuario. Otras funcionalidades pueden ser añadidas en el futuro (por eso se ha implementado en Django, garantizando una estructura coherente del proyecto y sus aplicaciones).
+
+
+###Sus funciones básicas son:
+
+- Realizar operaciones de creación/lectura/actualización/eliminación (CRUD) en los departamentos, cursos y alumnos (individualmente o en lotes).
+
+- Poder subir y descargar listas de alumnos por lotes en archivos con los siguientes formatos: Excel (.xslx, .xls), LibreOfficeCalc (.ods), archivos de texto plano separado por comas (.csv) o bien en de base de datos archivos sqlite.
+
+- Crear un tutorial interactivo para cada aplicación.
+
+###Lista de aplicaciones usadas
+
+- UsersData (aplicación para crear los modelos utilizados en todo el proyecto)      ESTADO: HECHO
+- AdminDashboard (el panel principal del administrador)         ESTADO: POR HACER
+- Portal (el portal para entrar y que será la página de inicio) ESTADO: POR HACER
+- Dashboard (el panel principal del usuario normal y de los "maestros").                                                    ESTADO: POR HACER
+- UserBrowser (aplicación utilizada para realizar búsquedas de cualquier
+para buscar a otros usuarios) ESTADO: POR HACER
+
+###Lista de modelos esenciales utilizados
+
+- CourseType        ESTADO: HECHO
+- Course            ESTADO: HECHO
+- UserProfile       ESTADO: HECHO
+
+
+
+
+#ENGLISH:
+---
+##General view
+
+Workshops is a project implemented in Python (version 3.8.x) using Django (version 2.2.x) (based on a previous implementation in PHP) that consists of a project composed of applications that respectively: Allow the management of lists of students in courses that are classified by departments, Allow logging in as a student, teacher or administrator, and one that shows a control panel or home page according to the role that each user has. Other functionalities can be added in the future (that's why it has been implemented in Django, ensuring a coherent structure of the project and its applications).
+
+###The basic functions are:
+
+- Perform creation/reading/updating/deleting (CRUD) operations on departments, courses and students (individually or in batches).
+
+- To be able to upload and download batch student lists in files with the following formats: Excel (.xslx, .xls), LibreOfficeCalc (.ods), comma separated flat text files (.csv) or in database sqlite files.
+
+- Create an interactive tutorial for each application.
+
+###List of apps used
+
+- UsersData (app to create the models used in whole the project).  STATE: DONE
+- AdminDashboard (the administrator's main panel)                  STATE: TODO
+- Portal (the portal to log in and which will be the home page)    STATE: TODO
+- Dashboard (the main panel of the normal user and the 'teachers').STATE: TODO
+- UserBrowser (application used to perform searches for any
+user to search for other users)                                    STATE: TODO
+
+###List of esential models used
+
+- CourseType  STATE: DONE
+- Course      STATE: DONE
+- UserProfile STATE: DONE
---
+++ UsersData/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+
+# Create your models here.
+class CourseType (models.Models):
+    '''
+    This model represents each classification ('classification' and 'CourseType' 
+    can be considered as synonims in this context from here onwards)  for all 
+    courses registered and it have a one-to-many relation with the model 
+    'Course', this model have only 3 properties, the default CourseType 
+    of all courses at first will be the Type called 'general'with id 1, it 
+    is declared in workshops_project/UsersData/models.py (in this file).
+    '''
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name="Classification's Name", help_text="This name field is to identify easily each course by the users (in addition to its id number), its length is 30 alphanumerical characters as maximum,and this is mandatory",max_length=30,blank=False,null=False)
+    overview = models.CharField(verbose_name="Classification's Overview", help_text="This field is to give a brief overview about this classification in a maximum of 200 alphanumerical characters, and this is not mandatory",max_length=200)
+
+
+# 'general' is the first instance of CourseType model and this is the default 
+# CourseType for all Courses  MUST NOT BE DELETED for major consistency of 
+# classification and management of the courses.
+general = CourseType(name="general",overview="This is the default Classification for all courses without a specific one")
+general.save()
+
+
+class Course(models.Model):
+    '''
+    This model represents each course in the project and its basic data
+    as its id, name, brief overview, tags (implemented with a third-party 
+    plugin called taggit which documentation is in 
+    https://django-taggit.readthedocs.io/en/latest/) to improve the 
+    localization of courses, a quota field (as maximum number of students,
+    if there is a limit and if there's no limit then will be set to zero 
+    that is the default value), and its relative 'classification' refered here in 
+    the project as 'CourseType'and it is represented here as the 'course_type'
+    field and it have a one-to-many relationship with CourseType 
+    (one CourseType to many Courses) and the default CourseType of each course 
+    is 'general' defined in this file before (if other CourseType is assigned and
+    later this is deleted, the CourseType of the 'orphan' course will be 'general'
+    again). And contents, learning materials, links of contents, teachers's comments,
+    information of relative contents (nor here but all these can be added as the 
+    project continues).
+    '''
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name="Course's Name", help_text="This name field is to identify easily each course by the users (in addition to its id number), its length is 30 alphanumerical characters as maximum,and this is mandatory",max_length=30,blank=False,null=False)
+    overview = models.CharField(verbose_name="Course's Overview", help_text="This field is to give a brief overwiew about this course and its details in a maximum of 200 alphanumerical characters, and this is not mandatory",max_length=200)
+    tags = TaggableManager()
+    quota = models.IntegerField(help_text="The quota field defines the maximum number of students allowed in this course, but if this value is not set or is zero, the course will not have a mandatory limit of students",default=0)
+    course_type = models.ForeignKey(CourseType, on_delete = models.SET_DEFAULT, default = general, help_text="This field indicates the classification of this course, if this field is empty, the default classification would be 'general' classification")
+
+class UserProfile:
+    '''
+    This model represents each user in 'workshops' projects and its apps
+    and is implemented by django.contrib.auth.models.* package default in 
+    Django, it is documented in the link bellow:
+    https://docs.djangoproject.com/en/2.2/ref/contrib/auth/
+    and to create a superuser (an administrator) it can be implemented 
+    with the automatic admin interface (in CLI or in a Web view) 
+    provided by Django,  it is documented in the link bellow:
+    https://docs.djangoproject.com/en/2.2/ref/contrib/admin/
+    '''
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)

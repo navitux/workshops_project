d1915e2de406c1107afd4972c5042fee022cc04e
Ivan Robles <navitux@disroot.org>
2020-12-12 17:34:09 -0600

    Changes fixing a stupid error in workshops/settings.py and updates to docs/todo.txt

--- workshops/settings.py
+++ workshops/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
+    'UsersData',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'UsersData',
 ]
 
 ROOT_URLCONF = 'workshops.urls'

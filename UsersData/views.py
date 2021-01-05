22693210ac0e35d8deb23933978749fe6afc1211
Ivan Robles <navitux@disroot.org>
2021-01-05 17:21:07 -0600

    Some changes in dashboard panel and index and base templates to improve the design and guides to fill fields correctly

--- UsersData/views.py
+++ UsersData/views.py
@@ -5,7 +5,11 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/signupin/')
 def dashboard(request):
+    #Function only to show the dashboard page.
     return render(request,"UsersData/dashboard.html")
+
+def create_coursetype(request):
+    pass
 
 def create_course(request):
     pass

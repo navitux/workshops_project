91266066567f520864c4a3943b79dfc8cb88b734
Ivan Alberto Robles Mosqueda <navitux@disroot.org>
2021-05-09 01:58:13 -0500

    Some minor optimizations and corrections were made in the front-end.

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,5 @@
 /*
+./templates/Courses/update.html javascript front-end mechanics goes here:
 This functions are a support to avoid undesired behaviours on creation or
 modification of course's overview
 */
@@ -16,3 +17,27 @@ $(document).ready(function(){
     document.getElementById("new_overview").value=new_text;
   });
 });
+
+/*
+./templates/SignUpIn/signupin.html javascript front-end mechanics goes here:
+*/
+var idies;
+var content;
+function tippies(idies,content){
+  tippy(idies, {
+    content: content,
+    placement: 'right-start',
+    interactive: true,
+  });
+}
+tippies('#help-username_r','Your username must be unique and its maximum\
+                          length is of 150 alphanumerical characters');
+tippies('#help-firstname','Your first name(s) can have as maximum of\
+                          150 alphanumerical characters of length');
+tippies('#help-lastname','Your last name(s) can have as maximum of\
+                          150 alphanumerical characters of length');
+tippies('#help-email','This field is not mandatory but it can help to\
+                     others users and administrators to get in touch with\
+                     you');
+tippies('#help-password1','Is recommended that your password have 10 or \
+                         more alphanumerical characters');
